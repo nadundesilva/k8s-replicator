f0ac1587f701f96d2858e5792b42bfde34709536
Nadun De Silva <nadunrds@gmail.com>
2022-03-17 13:32:22 +0530

    Add kubernetes client

---
+++ pkg/kubernetes/client.go
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022, Deep Net. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package kubernetes
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+type client struct {
+	clientset *kubernetes.Clientset
+}
+
+var _ Interface = (*client)(nil)
+
+func NewFromKubeConfig(kubeConfigPath string) *client {
+	// use the current context in kubeconfig
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return &client{
+		clientset: clientset,
+	}
+}
